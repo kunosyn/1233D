7b182b57870154b059d25adf52ce265b792ef150
kunosyn <65258314+kunosyn@users.noreply.github.com>
2021-10-26 15:57:28 -0500

    Added intake control.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace vex;
     right motor group ports - 6, 1
     left motor group ports - 4, 10
     arm group ports - 17, 18
+    intake port - 8
 */
 
 // FUNCTIONS
@@ -31,7 +32,7 @@ int main()
         rightMotorGroup.setVelocity(Controller1.Axis3.position() - Controller1.Axis1.position(), percent);
         leftMotorGroup.setVelocity(Controller1.Axis3.position() + Controller1.Axis1.position(), percent);
 
-        int armGroup_velocity = armGroup.velocity(percent);
+        int armGroup_velocity = armGroup.velocity(percent), intake_motor_velocity = intake_motor.velocity(percent);
 
         if (Controller1.ButtonL1.pressing()) { // If controller L1 is being actively pulled.
             // Sets velocity to the current velocity + 10. (moves arm up)
@@ -45,5 +46,13 @@ int main()
             armGroup.setStopping(brake); 
             armGroup.stop();
         };
+
+        if (Controller1.ButtonR1.pressing()) {
+            intake_motor.spin(forward);
+            intake_motor.setVelocity(intake_motor_velocity + 10);
+        } else {
+            intake_motor.setStopping(brake);
+            intake_motor.stop();
+        }
     } while (true);
 }
